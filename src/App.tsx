c83c147545c9a1b5665f684e61b0334656fac5cd
Astik <astemir.tlustangelov@gmail.com>
2023-11-17 11:03:34 +0300

    added theme, contenxt

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes, Link } from "react-router-dom"
 import { Suspense } from "react"
-import './index.scss'
+import './styles/index.scss'
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
+import { useTheme } from "./theme/useTheme"
+
 
 export const App = () => {
+    const { theme, toggleTheme } = useTheme()
 
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
             <Link to={'/'}>main</Link>
             <Link to={'./about'}>about</Link>
+            <button onClick={toggleTheme}>Toggle</button>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                         <Route path={'/about'} element={<AboutPageAsync />}/>
