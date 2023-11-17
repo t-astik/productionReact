7461fa0f351c35bc8c634cdabc41f9d5e875ae4d
Astik <astemir.tlustangelov@gmail.com>
2023-11-17 11:40:51 +0300

    classnames

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,12 @@
+type Mods = Record<string, string | boolean>
+
+export const classNames = (cls: string, mods: Mods, additional: string[]): string => {
+
+    return [
+        cls,
+        ...additional,
+        ...Object.entries(mods)
+            .filter(([className, value]) => value === true)
+            .map(([className]) => className)
+    ]. join(' ')
+}
